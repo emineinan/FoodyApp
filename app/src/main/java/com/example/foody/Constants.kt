3375a59c63c87322335388cd96461d1499a4c21b
Emine İnan <emineinanx@gmail.com>
2021-10-24 11:17:59 +0300

    Create Constants class

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "968eb0f91935470fa80d9c090f29316e"
+    }
+}
