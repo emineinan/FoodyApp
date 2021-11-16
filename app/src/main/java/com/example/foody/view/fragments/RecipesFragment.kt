00487a482aac051470ae6f1996417483cb1f0227
Emine İnan <emineinanx@gmail.com>
2021-11-16 19:58:29 +0300

    Create search menu

--- app/src/main/java/com/example/foody/view/fragments/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/view/fragments/RecipesFragment.kt
@@ -2,9 +2,8 @@ package com.example.foody.view.fragments
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -27,7 +26,7 @@ import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private val args by navArgs<RecipesFragmentArgs>()
 
@@ -45,6 +44,7 @@ class RecipesFragment : Fragment() {
     ): View? {
         _binding = FragmentRecipesBinding.inflate(inflater, container, false)
 
+        setHasOptionsMenu(true)
         setAdapter()
 
         recipesViewModel.readBackOnline.observe(viewLifecycleOwner, {
@@ -125,6 +125,23 @@ class RecipesFragment : Fragment() {
         binding.shimmerRecyclerView.adapter = recipesAdapter
         binding.shimmerRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         showShimmerEffect()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.recipes_menu, menu)
+
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 
     private fun showShimmerEffect() {
