15b6ab999367136149837eefa4627f159ecfc9f6
Emine İnan <emineinanx@gmail.com>
2021-11-16 20:31:11 +0300

    Handle search response

--- app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
@@ -32,9 +32,14 @@ class MainViewModel @Inject constructor(
 
     // RETROFIT
     var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+    var searchRecipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
 
     fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
         getRecipesSafeCall(queries)
+    }
+
+    fun searchRecipes(searchQuery: Map<String, String>) = viewModelScope.launch {
+        searchRecipesSafeCall(searchQuery)
     }
 
     private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
@@ -56,7 +61,20 @@ class MainViewModel @Inject constructor(
         } else {
             recipesResponse.value = NetworkResult.Error("No Internet Connection!")
         }
+    }
 
+    private suspend fun searchRecipesSafeCall(searchQuery: Map<String, String>) {
+        searchRecipesResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.searchRecipes(searchQuery)
+                searchRecipesResponse.value = handleFoodRecipesResponse(response)
+            } catch (e: Exception) {
+                searchRecipesResponse.value = NetworkResult.Error("Recipes not found.")
+            }
+        } else {
+            searchRecipesResponse.value = NetworkResult.Error("No Internet Connection!")
+        }
     }
 
     private fun offlineCacheRecipes(foodRecipe: FoodRecipe) {
