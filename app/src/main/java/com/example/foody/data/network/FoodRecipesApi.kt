c2b9731bc0e84424e118fdc1ba243a878fde3d81
Emine İnan <emineinanx@gmail.com>
2021-11-16 20:10:27 +0300

    Add search function to Api class

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -11,4 +11,9 @@ interface FoodRecipesApi {
     suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String, String>
+    ): Response<FoodRecipe>
 }
