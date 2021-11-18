51017602a3bf56b99d1d80279f2d91ee7f94deaa
Emine İnan <emineinanx@gmail.com>
2021-11-19 00:09:18 +0300

    Display recipe data in overview

--- app/src/main/java/com/example/foody/view/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/foody/view/fragments/overview/OverviewFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import coil.load
+import com.example.foody.R
 import com.example.foody.databinding.FragmentOverviewBinding
-
+import com.example.foody.models.Result
 
 class OverviewFragment : Fragment() {
     private var _binding: FragmentOverviewBinding? = null
@@ -17,6 +22,106 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        binding.imageViewMain.load(myBundle?.image)
+        binding.textViewOverviewTitle.text = myBundle?.title
+        binding.textViewOverviewFavorites.text = myBundle?.aggregateLikes.toString()
+        binding.textViewOverviewDuration.text = myBundle?.readyInMinutes.toString()
+        binding.textViewSummary.text = myBundle?.summary
+
+        if (myBundle?.vegetarian == true) {
+            binding.imageViewOverviewVegetarian.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewVegetarian.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.vegan == true) {
+            binding.imageViewOverviewVegan.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewVegan.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.glutenFree == true) {
+            binding.imageViewOverviewGlutenFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewGlutenFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.dairyFree == true) {
+            binding.imageViewOverviewDairyFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewDairyFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.veryHealthy == true) {
+            binding.imageViewOverviewHealthy.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewHealthy.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.cheap == true) {
+            binding.imageViewOverviewCheap.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewOverviewCheap.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
         return binding.root
     }
 }
