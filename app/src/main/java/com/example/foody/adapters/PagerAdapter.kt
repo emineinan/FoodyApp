644ac24c168e91e48e803076702fa09dbb542c0e
Emine İnan <emineinanx@gmail.com>
2021-11-18 22:17:58 +0300

    Create PagerAdapter class

---
+++ app/src/main/java/com/example/foody/adapters/PagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.foody.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapter(
+    private val resultBundle: Bundle,
+    private val fragments: ArrayList<Fragment>,
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        fragments[position].arguments = resultBundle
+        return fragments[position]
+    }
+}
--- app/src/main/java/com/example/foody/view/DetailsActivity.kt
+++ app/src/main/java/com/example/foody/view/DetailsActivity.kt
@@ -1,15 +1,23 @@
 package com.example.foody.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.example.foody.R
+import com.example.foody.adapters.PagerAdapter
 import com.example.foody.databinding.ActivityDetailsBinding
-import com.example.foody.databinding.ActivityMainBinding
+import com.example.foody.view.fragments.ingredients.IngredientsFragment
+import com.example.foody.view.fragments.instructions.InstructionsFragment
+import com.example.foody.view.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
@@ -19,6 +27,34 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewPager.isUserInputEnabled = false
+        binding.viewPager.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
