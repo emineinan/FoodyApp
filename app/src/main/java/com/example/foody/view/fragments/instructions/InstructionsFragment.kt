8386e4dd4431d6084abced2723e38635a9b0c075
Emine İnan <emineinanx@gmail.com>
2021-11-20 23:02:50 +0300

    Modify InstructionsFragment

--- app/src/main/java/com/example/foody/view/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/view/fragments/instructions/InstructionsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.foody.databinding.FragmentInstructionsBinding
+import com.example.foody.models.Result
+import com.example.foody.util.Constants
+import com.example.foody.util.Constants.Companion.RECIPE_RESULT
 
 class InstructionsFragment : Fragment() {
     private var _binding: FragmentInstructionsBinding? = null
@@ -16,6 +20,14 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT)
+
+        binding.webViewInstructions.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.webViewInstructions.loadUrl(websiteUrl)
+
         return binding.root
     }
 }
