a2282ec7f7f48424968b3f240f136e4efa8e08e4
Emine İnan <emineinanx@gmail.com>
2021-11-21 15:16:18 +0300

    Modify RecipesDatabase class

--- app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
@@ -3,12 +3,17 @@ package com.example.foody.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.foody.data.database.entities.FavoritesEntity
 import com.example.foody.data.database.entities.RecipesEntity
 
-@Database(entities = [RecipesEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [RecipesEntity::class, FavoritesEntity::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(RecipesTypeConverter::class)
 abstract class RecipesDatabase : RoomDatabase() {
 
     abstract fun recipesDao(): RecipesDao
-    
+
 }
