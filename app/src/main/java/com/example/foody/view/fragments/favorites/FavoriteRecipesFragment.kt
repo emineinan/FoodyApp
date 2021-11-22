cb8afd71f5e015830a41c313c19eee8a509536cb
Emine İnan <emineinanx@gmail.com>
2021-11-22 11:44:33 +0300

    Add FavoriteRecipesFragment

--- app/src/main/java/com/example/foody/view/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/view/fragments/favorites/FavoriteRecipesFragment.kt
@@ -5,15 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foody.adapters.FavoriteRecipesAdapter
 import com.example.foody.databinding.FragmentFavoriteRecipesBinding
+import com.example.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
     private var _binding: FragmentFavoriteRecipesBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private val favoriteRecipesAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.favoriteRecipesAdapter = favoriteRecipesAdapter
+
+        setAdapter()
+
         return binding.root
+    }
+
+    private fun setAdapter() {
+        binding.recyclerViewFavoriteRecipes.adapter = favoriteRecipesAdapter
+        binding.recyclerViewFavoriteRecipes.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
