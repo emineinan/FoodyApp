3d08190329bb7b02b8fa66a0b10d797cc3b419fc
Emine İnan <emineinanx@gmail.com>
2021-11-24 21:58:55 +0300

    Fix internet connection bug

--- app/src/main/java/com/example/foody/view/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/view/fragments/recipes/RecipesFragment.kt
@@ -54,7 +54,7 @@ class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
             recipesViewModel.backOnline = it
         })
 
-        lifecycleScope.launch {
+        lifecycleScope.launchWhenStarted {
             networkListener = NetworkListener()
             networkListener.checkNetworkAvailability(requireContext()).collect { status ->
                 Log.d("NetworkListener", status.toString())
