516ae01bdc9bbc7a7ad3d233669312eaee4f3abc
Emine İnan <emineinanx@gmail.com>
2021-11-26 23:01:49 +0300

    Clean FavoriteRecipeBinding code

--- app/src/main/java/com/example/foody/bindingadapters/FavoriteRecipeBinding.kt
+++ app/src/main/java/com/example/foody/bindingadapters/FavoriteRecipeBinding.kt
@@ -1,8 +1,8 @@
 package com.example.foody.bindingadapters
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foody.adapters.FavoriteRecipesAdapter
@@ -12,38 +12,22 @@ class FavoriteRecipeBinding {
 
     companion object {
 
-        @BindingAdapter("viewVisibility", "setData", requireAll = false)
+        @BindingAdapter("setVisibility", "setData", requireAll = false)
         @JvmStatic
-        fun setDataAndViewVisibility(
+        fun setVisibility(
             view: View,
             favoritesEntity: List<FavoritesEntity>?,
             favoriteRecipesAdapter: FavoriteRecipesAdapter?
         ) {
-            if (favoritesEntity.isNullOrEmpty()) {
-                when (view) {
-                    is ImageView -> {
-                        view.visibility = View.VISIBLE
-                    }
-                    is TextView -> {
-                        view.visibility = View.VISIBLE
-                    }
-                    is RecyclerView -> {
-                        view.visibility = View.INVISIBLE
+            when (view) {
+                is RecyclerView -> {
+                    val dataCheck = favoritesEntity.isNullOrEmpty()
+                    view.isInvisible = dataCheck
+                    if (!dataCheck) {
+                        favoritesEntity?.let { favoriteRecipesAdapter?.setData(it) }
                     }
                 }
-            } else {
-                when (view) {
-                    is ImageView -> {
-                        view.visibility = View.INVISIBLE
-                    }
-                    is TextView -> {
-                        view.visibility = View.INVISIBLE
-                    }
-                    is RecyclerView -> {
-                        view.visibility = View.VISIBLE
-                        favoriteRecipesAdapter?.setData(favoritesEntity)
-                    }
-                }
+                else -> view.isVisible = favoritesEntity.isNullOrEmpty()
             }
         }
     }
